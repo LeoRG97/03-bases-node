0b9a026677e9503a96f12b1d0675fdada7323eb8
Leonardo Rodriguez <residenciasbigpink@girasolo.com>
2020-06-12 18:07:30 -0500

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,20 @@
+const defaultOptions = {
+    base: {
+        demand: true,
+        alias: 'b',
+    },
+    limite: {
+        alias: 'l',
+        default: 10,
+    }
+};
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', defaultOptions)
+    .command('listar', 'Genera un archivo con la tabla de multiplicar', defaultOptions)
+    .help()
+    .argv;
+
+module.exports = {
+    argv,
+};
